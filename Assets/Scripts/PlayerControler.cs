269398b2bec7cd48f98c7664a0bb0e271cd62eb4
P01M4S <pmas@ctsformacion.com>
2025-10-08 13:09:54 +0200

    Movimiento, plataformas y mas

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,14 +1,16 @@
 using System;
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.TextCore;
 
 public class PlayerControler : MonoBehaviour
 {
     public CharacterController _controller;
     public InputAction _moveAction;
     public Vector2 _moveInput;
-    public float _movementSpeed = 6;
+    public float _movementSpeed = 3;
     public float _gravity = -9;
     [SerializeField] private Vector3 _playerGravity;
     public Transform _sensor;
@@ -17,13 +19,20 @@ public class PlayerControler : MonoBehaviour
     public float _jumpHeigth = 2;
     public InputAction _jumpAction;
     public float _smoothTime = 0.2f;
-    public float _turnSmooth;
+    private float _turnSmooth;
+    public InputAction _lookAction;
+    public Vector2 _lookImput;
+    public Transform _mainCamara;
+    public InputAction _aimAction;
 
     void Awake()
     {
         _controller = GetComponent<CharacterController>();
         _moveAction = InputSystem.actions["Move"];
         _jumpAction = InputSystem.actions["Jump"];
+        _lookAction = InputSystem.actions["Look"];
+        _mainCamara = Camera.main.transform;
+        _aimAction = InputSystem.actions["Aim"];
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,7 +43,19 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovimientoCutre();
+        _moveInput = _moveAction.ReadValue<Vector2>();
+        _lookImput = _lookAction.ReadValue<Vector2>();
+        //MovimientoCutre();
+        Movement();
+        AimMovement();
+        if (_aimAction.IsInProgress())
+        {
+            AimMovement();
+        }
+        else
+        {
+            Movement();
+        }
         Gravity();
         if (_jumpAction.WasPressedThisFrame() && IsGrounded())
         {
@@ -42,9 +63,60 @@ public class PlayerControler : MonoBehaviour
         }
     }
 
+    void AimMovement()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+        
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamara.eulerAngles.y;
+        float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _mainCamara.eulerAngles.y, ref _turnSmooth, _smoothTime);
+
+        transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+        if (direction != Vector3.zero)
+        {
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            _controller.Move(moveDirection.normalized * _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        if (direction != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamara.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmooth, _smoothTime);
+
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            
+            _controller.Move(moveDirection.normalized * _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    /*void Movimiento2()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+        Ray ray = Camera.main.ScreenPointToRay(_lookImput);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Vector3 playerForeard = hit.point - transform.position;
+            playerForeard.y = 0;
+            transform.forward = playerForeard;
+        }
+        
+        if (direction != Vector3.zero)
+        {
+            _controller.Move(direction.normalized * _movementSpeed * Time.deltaTime);
+        }
+    }
+
     void MovimientoCutre()
     {
-        _moveInput = _moveAction.ReadValue<Vector2>();
         Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
         if (direction != Vector3.zero)
         {
@@ -54,7 +126,7 @@ public class PlayerControler : MonoBehaviour
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmooth, _smoothTime);
             transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
         }
-    }
+    }*/
 
     void Gravity()
     {
